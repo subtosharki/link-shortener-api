0955ab90e2eb96afb39152ba38140eda665b9357
subtosharki <camywam@yahoo.com>
2022-11-12 20:01:51 -0500

    omg im dumb

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,13 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(process.env.PORT || 3000);
   const options = new DocumentBuilder()
     .setTitle('Shorten URL API')
     .setDescription('The Shorten URL API documentation')
     .setVersion('1.0')
-    .addTag('shorten-url')
     .build();
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api-docs', app, document);
+  SwaggerModule.setup('/docs', app, document);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
